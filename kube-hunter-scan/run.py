98835a4a65af3f1deefc11c57a4f0a4088e0c248
Max <max@neuvians.net>
2019-05-21 15:45:03 -0400

    Wrap in main fuction

--- kube-hunter-scan/run.py
+++ kube-hunter-scan/run.py
@@ -5,43 +5,47 @@ import subprocess
 import uuid
 import yaml
 
-origin = os.getenv("ORIGIN", "Missing origin")
-component = os.getenv("COMPONENT", "Missing componet")
-description = os.getenv("DESCRIPTION", "Missing description")
-path = os.getenv("OUT_PATH", "/checks/")
-satisfies = os.getenv("SATISFIES", "")
-release = os.getenv("RELEASE", False)
-domain = os.getenv("DOMAIN_NAME", False)
+def main():
+    origin = os.getenv("ORIGIN", "Missing origin")
+    component = os.getenv("COMPONENT", "Missing componet")
+    description = os.getenv("DESCRIPTION", "Missing description")
+    path = os.getenv("OUT_PATH", "/checks/")
+    satisfies = os.getenv("SATISFIES", "")
+    release = os.getenv("RELEASE", False)
+    domain = os.getenv("DOMAIN_NAME", False)
 
-if domain:
-    output = subprocess.check_output(["./kube-hunter.py", "--remote", domain, "--report", "yaml", "--log", "NONE"])
-else:
-    output = subprocess.check_output(["./kube-hunter.py", "--pod", "--report", "yaml", "--log", "NONE"])
-    
-obj = yaml.load(output, Loader=yaml.FullLoader)
+    if domain:
+        output = subprocess.check_output(["./kube-hunter.py", "--remote", domain, "--report", "yaml", "--log", "NONE"])
+    else:
+        output = subprocess.check_output(["./kube-hunter.py", "--pod", "--report", "yaml", "--log", "NONE"])
+        
+    obj = yaml.load(output, Loader=yaml.FullLoader)
 
-if len(obj["vulnerabilities"]) == 0:
-    passed = True
-else:
-    passed = False
+    if len(obj["vulnerabilities"]) == 0:
+        passed = True
+    else:
+        passed = False
 
-vulnerabilities = map(lambda v: v["category"] + ": " + v["vulnerability"] + " - " + v["description"], obj["vulnerabilities"])   
+    vulnerabilities = map(lambda v: v["category"] + ": " + v["vulnerability"] + " - " + v["description"], obj["vulnerabilities"])   
 
-check = {
-    "origin": origin,
-    "component": component,
-    "description": description,
-    "satisfies": satisfies.split(","),
-    "release": release,
-    "passed": passed,
-    "timestamp": datetime.datetime.utcnow().isoformat("T") + "Z",
-    "references": json.dumps(vulnerabilities)
-}
+    check = {
+        "origin": origin,
+        "component": component,
+        "description": description,
+        "satisfies": satisfies.split(","),
+        "release": release,
+        "passed": passed,
+        "timestamp": datetime.datetime.utcnow().isoformat("T") + "Z",
+        "references": json.dumps(vulnerabilities)
+    }
 
-print check
+    print check
 
-if release:
-    file_name = path + str(uuid.uuid4()) + ".json"
-    f = open(file_name, "a")
-    f.write(json.dumps(check))
-    f.close()
+    if release:
+        file_name = path + str(uuid.uuid4()) + ".json"
+        f = open(file_name, "a")
+        f.write(json.dumps(check))
+        f.close()
+
+if __name__== "__main__":
+     main()
